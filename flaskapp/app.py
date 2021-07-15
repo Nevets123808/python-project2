50874f7cdc2852ca3b8944ac82934a0903344e48
Steven Bourn <stevenbourn2@hotmail.com>
2021-07-15 10:08:22 +0000

    basic SQL structure

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,6 +6,24 @@ import requests
 app = Flask(__name__)
 
 app.config("SQL_ALCHEMY_DATABASE_URI") = getenv("DATABASE_URI")
+app.config("SQL_ALCHEMY_SECRET_KEY") = getenv("SECRET_KEY")
+app.config("SQL_ALCHEMY_TRACK_MODIFICATIONS") = False
+
+db=SQLAlchemy(app)
+
+class characters(db.Model):
+    ID = db.Column(db.Integer, primary_key = True)
+    Strength = db.Column(db.Integer)
+    Perception = db.Column(db.Integer)
+    Endurance = db.Column(db.Integer)
+    Charisma = db.Column(db.Integer)
+    Intelligence = db.Column(db.Integer)
+    Agility = db.Column(db.Integer)
+    Luck = db.Column(db.Integer)
+    Tag1 = db.Column(db.String(20))
+    Tag2 = db.Column(db.String(20))
+    Tag3 = db.Column(db.String(20))
+
 @app.route('/')
 @app.route('/home')
 def home():
---
+++ flaskapp/create.py
@@ -0,0 +1,4 @@
+from app import app,db
+
+db.drop_all()
+db.create_all()
