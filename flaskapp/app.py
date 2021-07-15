0726c6b0756662c530b78dd0ef2c51682b1228dd
Steven Bourn <stevenbourn2@hotmail.com>
2021-07-15 09:28:53 +0000

    Flask app testing

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
+from flask-sqlalchemy import SQLAlchemy
+from os import getenv
 import requests
 
 app = Flask(__name__)
 
+app.config("SQL_ALCHEMY_DATABASE_URI") = getenv("DATABASE_URI")
 @app.route('/')
 @app.route('/home')
 def home():
---
+++ flaskapp/tests/test_unit.py
@@ -0,0 +1,35 @@
+from flask import url_for, jsonify
+from flask_testing import TestCase
+import requests_mock
+
+from app import app
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class test_skilltag(TestBase):
+    def test_response(self):
+        with requests_mock.Mocker() as mocker:
+            mocker.get("http://specialgen:5000/genspecial", json = {"S":5,"P":5,"E":5,"C":5,"I":5,"A":5,"L":5})
+            mocker.get("http://skilltag:5000/skilltag", json = {"tag1":"Small Guns","tag2":"Big Guns", "tag3":"Science"})
+            mocker.post("http://sheetmake:5000/sheetmake", json = {"Small Guns": 60,
+        "Big Guns": 35,
+        "Energy Weapons": 15,
+        "Unarmed": 70,
+        "Melee Weapons": 60,
+        "Throwing": 45,
+        "First Aid": 35,
+        "Doctor": 20,
+        "Sneak": 25,
+        "LockPick": 25,
+        "Steal": 25,
+        "Traps": 25,
+        "Science": 55,
+        "Repair": 25,
+        "Speech": 35,
+        "Barter": 30,
+        "Gambling": 35,
+        "Outdoorsman": 10,})
+            response = self.client.get(url_for("home"))
+            self.assertEqual(200,response.status_code)
+            self.assertIn(b'"Small Guns":60', response.data)
